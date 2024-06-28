97da272f8e76eacd3b3f345641d7df243d56a089
sprklpixl <shandell@tutamail.com>
2024-06-27 19:29:40 -0500

    added validations for both tables

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :category, optional: true
+
+  # Validations
+  validates :product_id, presence: true
+  validates :title, presence: true
+  validates :type, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :inventory, numericality: { greater_than_or_equal_to: 0 }
 end
