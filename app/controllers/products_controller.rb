7d649d9a816bdad94d6f2f33a44ed7c2524aec92
sprklpixl <shandell@tutamail.com>
2024-07-08 20:22:24 -0500

    first attempt at pagination, undefinted method 'page' for class Product in ProductsController#index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    # @products = Product.all
+    @products = Product.page(params[:page]).per(10)
   end
 
   def show
     @product = Product.find_by(id: params[:id])
+    # @product = Product.find(params[:id])
     if @product.nil?
       flash[:alert] = "Product not found."
       redirect_to index_path
